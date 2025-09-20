032632b1b967176abd71810f40f0c30cbb8a86a4
BryceYoung <bryce.young18@icloud.com>
2025-09-20 19:02:10 +0930

    Typehinted optimisationAlgorithms.py

--- final/code/optimisationAlgorithms.py
+++ final/code/optimisationAlgorithms.py
@@ -1,10 +1,11 @@
+from typing import Sequence
 from ioh import get_problem, ProblemType, ProblemClass, logger
 import sys
 import numpy as np
 import random
 
 # EA(1+1) Algorithm
-def ea11(problem: ProblemType, budget: int | None = None) -> tuple[int,list[int]]:
+def ea11(problem: ProblemType, budget: int | None = None) -> tuple[float,list[int]]:
     # Set budget to 50n^2
     if budget is None:
         budget = int(problem.meta_data.n_variables * problem.meta_data.n_variables * 50)
@@ -15,7 +16,7 @@ def ea11(problem: ProblemType, budget: int | None = None) -> tuple[int,list[int]
         optimum: int = problem.optimum.y
     
     # Setup x & f
-    f_opt = sys.float_info.min
+    f_opt: float = sys.float_info.min
     x_opt: list[int] = list(np.random.randint(2, size = problem.meta_data.n_variables))
     for _ in range(budget):
         x: list[int] = x_opt.copy()
@@ -41,7 +42,7 @@ def ea11(problem: ProblemType, budget: int | None = None) -> tuple[int,list[int]
     return f_opt, x_opt
 
 # RLS
-def RLS(problem: ProblemType, budget=100000, n=100):
+def RLS(problem: ProblemType, budget: int = 100000, n: int = 100) -> tuple[float,list[int]]:
     # setup Problem Optimum
     if problem.meta_data.problem_id == 18 and problem.meta_data.n_variables == 32:
         optimum: int = 8
@@ -49,18 +50,18 @@ def RLS(problem: ProblemType, budget=100000, n=100):
         optimum: int = problem.optimum.y
     
     # Setup x & f
-    f_opt = sys.float_info.min
-    x_opt = []
+    f_opt: float = sys.float_info.min
+    x_opt: list[int] = []
     for i in range(n):
         x_opt.append(random.randint(0, 1))
     
     for i in range(budget):
-        x = x_opt.copy()
+        x: list[int] = x_opt.copy()
 
         # Flip one randomly chosen bit
-        randFlip = random.randint(0, n - 1)
+        randFlip: int = random.randint(0, n - 1)
         x[randFlip] = 1 - x[randFlip]
-        f = problem(x)
+        f: float = problem(x)
 
         # If better than optimum update optimum
         if f > f_opt:
