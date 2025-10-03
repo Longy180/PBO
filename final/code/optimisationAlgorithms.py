1a5a3accad3ddcb1e5ae61a5f071fc4f8a5088f4
Asad-i <140378212+Asad-i@users.noreply.github.com>
2025-10-03 18:51:19 +0930

    Added RS and problem testing code for Plots

--- final/code/optimisationAlgorithms.py
+++ final/code/optimisationAlgorithms.py
@@ -5,7 +5,7 @@ import numpy as np
 import random
 
 # EA(1+1) Algorithm
-def ea11(problem: ProblemType, budget: int | None = None) -> tuple[float,list[int]]:
+def ea11(problem: ProblemType, budget: int = 100000) -> tuple[float,list[int]]:
     # Set budget to 50n^2
     if budget is None:
         budget = int(problem.meta_data.n_variables * problem.meta_data.n_variables * 50)
@@ -74,3 +74,70 @@ def RLS(problem: ProblemType, budget: int = 100000, n: int = 100) -> tuple[float
             break
 
     return f_opt, x_opt
+
+def random_search(func, budget = 100000):
+    if budget is None:
+        budget = int(func.meta_data.n_variables * func.meta_data.n_variables * 50)
+
+    if func.meta_data.problem_id == 18 and func.meta_data.n_variables == 32:
+        optimum = 8
+    else:
+        optimum = func.optimum.y
+    print(optimum)
+    # 10 independent runs for each algorithm on each problem.
+    for r in range(10):
+        f_opt = sys.float_info.min
+        x_opt = None
+        for i in range(budget):
+            x = np.random.randint(2, size = func.meta_data.n_variables)
+            f = func(x)
+            if f > f_opt:
+                f_opt = f
+                x_opt = x
+            if f_opt >= optimum:
+                break
+        func.reset()
+    return f_opt, x_opt
+
+problem_ids = [1, 2, 3, 18, 23, 24, 25]
+dimension = 100
+budget = 100000
+num_runs = 10
+
+algorithms = [
+    (random_search, "RandomSearch"),
+    (RLS, "RLS"),
+    (ea11, "EA_1plus1")
+]
+
+# Run experiments for each algorithm across all problems
+for algorithm, algorithm_name in algorithms:
+
+    l = logger.Analyzer(
+        root="/Users/axvd/Downloads/", 
+        folder_name="run",
+        algorithm_name=algorithm_name, 
+        algorithm_info=f"Benchmark of {algorithm_name}"
+    )
+    
+    # Run this algorithm on all problems
+    for fid in problem_ids:
+        
+        # Create problem instance
+        problem = get_problem(
+            fid=fid, 
+            dimension=dimension, 
+            instance=1, 
+            problem_class=ProblemClass.PBO
+        )
+
+        problem.attach_logger(l)
+        
+        # Run algorithm 10 times on this problem
+        for run in range(num_runs):
+            algorithm(problem, budget=budget)
+            problem.reset() 
+        
+    
+    # Delete logger after this algorithm completes all problems
+    del l
